4227f0c1202c964b774d2343c8a0ec4ef81bf6d4
jakariacst318 <jsjakariacst@gmail.com>
2024-05-29 13:47:23 +0600

    privet route load-face spanner add

--- src/components/PrivetRoute/PrivetRoute.jsx
+++ src/components/PrivetRoute/PrivetRoute.jsx
@@ -1,16 +1,30 @@
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import { Navigate } from "react-router-dom";
+import { Circles } from 'react-loader-spinner';
 
-const PrivetRoute = ( { children } ) => {
+const PrivetRoute = ({ children }) => {
 
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
+    if (loading) {
+        return <span className="flex justify-center pt-16">
+            <Circles
+                height="80"
+                width="80"
+                color="#4fa94d"
+                ariaLabel="circles-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+            />
+        </span>
+    }
 
-    if(user){
+    if (user) {
         return children;
     }
-    
-    return <Navigate to='/login'></Navigate>;    
+
+    return <Navigate to='/login'></Navigate>;
 };
 
 export default PrivetRoute;
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -9,23 +9,28 @@ export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     
     
     const createUser = (email, password) =>{
+        setLoading(true)
         return createUserWithEmailAndPassword(auth,email, password)
     }
 
     const signInUser = (email,password) =>{
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
 
     const logOUt = () =>{
+        setLoading(true)
         return signOut(auth);
     }
 
  //  observe auth state change 
     useEffect(() =>{
      const unSubscribe =  onAuthStateChanged(auth, currentUser =>{
+        setLoading(false)
             setUser(currentUser);
             console.log('hello observing cunnet user !!!!!!!!', currentUser)
         });
@@ -38,6 +43,7 @@ const AuthProvider = ({ children }) => {
    
     const authInfo = { 
         user, 
+        loading,
         createUser, 
         signInUser,
         logOUt
